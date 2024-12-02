cbd11494263a7e6c47547837839307028dcf579b
Rotem Weintal <rotem.weintal@post.runi.ac.il>
2024-12-02 14:57:49 +0200

    Primes

--- Primes.java
+++ Primes.java
@@ -1,5 +1,22 @@
 public class Primes {
     public static void main(String[] args) {
-        // Replace this statement with your code
+        int n = Integer.parseInt(args[0]);
+        boolean[] isPrime = new boolean[n + 1];
+        for (int i = 2; i <= n; i++) { 
+            isPrime[i] = true;
+        }
+
+        for (int p = 2; p * p <= n; p ++) { 
+            if (isPrime[p]) { 
+                for (int i = p * p; i <= n; i += p) {
+                     isPrime[i] = false; 
+                    } 
+                } 
+            } 
+                     for (int p = 2; p <= n; p++) { 
+                        if (isPrime[p]) { 
+                            System.out.print(p + " "); 
+                        } 
+                    }
     }
 }
